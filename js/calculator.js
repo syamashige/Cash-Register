3f8e5ce0ac4deda907452d19251bb8c1fd28fc5e
Sarah <yamashige.sarah@gmail.com>
2018-06-25 20:28:37 -1000

    Still needs a lot of work

--- js/calculator.js
+++ js/calculator.js
@@ -112,21 +112,31 @@
     var getEquals = document.getElementById('equals');
     getEquals.addEventListener('click', showEquals);
 
+    var receiptElem = document.getElementById('receipt');
+
+    var total = [];
+
+    var totalElem = document.getElementById('total'); 
+
     function showEquals() {
 
             if(!!storeAddition) {
                 storeNums = parseFloat(displayScreen.innerHTML);
                 result = storeAddition + storeNums;
                 displayScreen.innerHTML = result;
+                total.push(result);
                 // console.log("add" + storeAddition);
                 // console.log("this" + storeNums);
                 // console.log("result" + result)
+                receiptElem.innerHTML += "Item " + " " + "$ " + result + " ";
+                console.log(total);
                 storeAddition = null;
-                receiptElem.appendChild(result);
+                
             } else if (!!storeSubtraction) {
                 storeNums = parseFloat(displayScreen.innerHTML);
                 result = storeSubtraction - storeNums;
                 displayScreen.innerHTML = result;
+                receiptElem.innerHTML += "Item " + " " + " " + " $ " + result + " ";
                 // console.log("sub" + storeSubtraction);
                 // console.log("minus" + storeNums);
                 // console.log("result" + result);
@@ -135,16 +145,21 @@
                 storeNums = parseFloat(displayScreen.innerHTML);
                 result = storeMultiplication * storeNums;
                 displayScreen.innerHTML = result;
+                receiptElem.innerHTML += "Item " + " " + "$ " + result + " ";
                 storeMultiplication = null;
             } else if (!!storeDivision) {
                 storeNums = parseFloat(displayScreen.innerHTML);
                 result = storeDivision / storeNums;
                 displayScreen.innerHTML = result;
+                receiptElem.innerHTML += "Item " + " " + "$ " + result + " ";
                 storeDivision = null;
             }
     }
 
-    var receiptElem = document.getElementById('receipt');
+
+    // var receiptElem = document.getElementById('receipt');
+
+
 
 
 
